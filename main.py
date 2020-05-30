8f197f53e8ac65ec34fd3cfd76d793ee3bcaae41
natanproj <natanaemartinsfelix@hotmail.com>
2020-05-30 15:51:27 -0300

    Removindo extra text

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import qtd_ports
 import serial
 import show_version
 import json
-result = open ('Result.txt', 'w')
 hostes = "BRSA-SWA-3511-01" #Verificar amanha as informações de hostname
 por = 'Model number                    : WS-C3560-24PS-E' #sh version | inc Model num
 seri = "System serial number            : CAT1037RJF7"
@@ -18,15 +17,5 @@ up.update(portasResult)
 up.update(serialResult)
 up.update(versionResult)
 info = json.dumps(up,ensure_ascii=False)
-result.write(info)
-result.close()
-"""
-ifo = json.dumps(hostnameResult, ensure_ascii=False)
-infor = json.dumps(portasResult, ensure_ascii=False)
-infos = json.dumps (serialResult, ensure_ascii=False)
-infosr = json.dumps (versionResult, ensure_ascii=False)
-#result.write("\n")
-result.write(ifo + infor + infos + infosr)
-"""
 
-#print(ifo + infor + infos + infosr )
+
