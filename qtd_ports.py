eb2bed76705df892d3b4b38c55a33c63be9fa7a7
natanproj <66176850+natanproj@users.noreply.github.com>
2020-06-22 10:57:14 -0300

    Delete qtd_ports.py

--- qtd_ports.py
+++
@@ -1,9 +0,0 @@
-def qtd_ports(por):
-    model = por
-    portas = []
-    for port in model:
-        if port.isdigit():
-            portas.append(port)
-    qtd = int(portas[-2] + portas[-1])
-    po = {'Ports': qtd}
-    return po
